72525165aecff2e951d7d2465804c3bffdf3b212
AqilAdam <aqil.adam.02004@gmail.com>
2025-03-02 19:54:44 +0530

    small mistake

--- app/db.py
+++ app/db.py
@@ -37,7 +37,7 @@ def create_tables():
             );
         """
         create_employees_table = """
-            CREATE TemployeesABLE IF NOT EXISTS employees (
+            CREATE TABLE employees IF NOT EXISTS employees (
                 employeeId VARCHAR(50) PRIMARY KEY,
                 employeeName VARCHAR(255) NOT NULL,
                 mobileNumber BIGINT,
